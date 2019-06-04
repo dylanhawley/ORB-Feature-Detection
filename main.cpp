e7d05f5360f67fe8c8074c118a16798cb4b8dcdb
Dylan Hawley <dylanthomashawley@gmail.com>
2019-06-04 11:17:16 -0400

    connects matches between two images

--- main.cpp
+++ main.cpp
@@ -2,29 +2,39 @@
 #include <opencv2/opencv.hpp>
 
 int main(int argc, char **argv) {
-    // Read reference image
-    std::string refFilename("reference_group.png");
-    std::cout << "Reading reference image : " << refFilename << std::endl;
-    cv::Mat imReference = cv::imread(refFilename);
+    // Read train image
+    std::string train_img_filename(argv[1]);
+    std::cout << "Reading reference image : " << train_img_filename << std::endl;
+    cv::Mat train_img = cv::imread(train_img_filename);
 
     // Read image to be analyzed
-    std::string imFilename("modified_group.png");
-    std::cout << "Reading image to align : " << imFilename << std::endl;
-    cv::Mat im = cv::imread(imFilename);
+    std::string query_img_filename(argv[2]);
+    std::cout << "Reading query image : " << query_img_filename << std::endl;
+    cv::Mat query_img = cv::imread(query_img_filename);
 
     cv::Ptr<cv::ORB> orb = cv::ORB::create();
 
-    std::vector<cv::KeyPoint> keypoints1;
-    cv::Mat descriptors1;
-    orb->detectAndCompute(imReference, cv::noArray(), keypoints1, descriptors1);
+    std::vector<cv::KeyPoint> train_img_keypoints, query_img_keypoints;
+    cv::Mat train_img_descriptors, query_img_descriptors;
 
-    std::vector<cv::KeyPoint> keypoints2;
-    cv::Mat descriptors2;
-    orb->detectAndCompute(im, cv::noArray(), keypoints2, descriptors2);
+    orb->detectAndCompute(train_img, cv::noArray(), train_img_keypoints, train_img_descriptors);
+    orb->detectAndCompute(query_img, cv::noArray(), query_img_keypoints, query_img_descriptors);
 
-    cv::drawKeypoints(im,keypoints2,imReference);
-    cv::namedWindow("Original", cv::WINDOW_AUTOSIZE);
-    cv::imshow("Original", imReference);
+    cv::BFMatcher bf(cv::NORM_HAMMING, true);
+    std::vector<cv::DMatch> matches;
+    bf.match(train_img_descriptors, query_img_descriptors, matches);
+
+    // Sort for good matches
+    std::sort(matches.begin(), matches.end());
+    std::vector<cv::DMatch> good_matches;
+    for(int i = 0; i < 10; i++)
+        good_matches.push_back(matches[i]);
+
+    cv::Mat img_matches;
+    cv::drawMatches(train_img, train_img_keypoints, query_img, query_img_keypoints,
+                    good_matches, img_matches);
+    cv::namedWindow("Matches", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Matches", img_matches);
     cv::waitKey();
 
     return 0;
